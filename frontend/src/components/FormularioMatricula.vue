79916093e539e10f49dce37d24b769b2a23dbfe9
LuccaGarDal <luccagarciarosa@gmail.com>
2026-02-23 16:05:49 -0300

    criado estilo css para o front

--- frontend/src/components/FormularioMatricula.vue
+++ frontend/src/components/FormularioMatricula.vue
@@ -57,20 +57,108 @@ async function enviarFormulario () {
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <h1>Cadastro de usuários</h1>
     <form @submit.prevent="enviarFormulario">
-      <input type="text" v-model="form.nome" placeholder="Nome completo">
-      <br>
-      <input type="text" v-model="form.email" placeholder="E-mail">
-      <br>
-      <select v-model.number="form.cursoId" :disabled="carregandoCursos">
-        <option :value="null" disabled>Selecione o curso</option>
-        <option v-for="curso in cursos" :key="curso.id" :value="curso.id">
-          {{ curso.nome }}
-        </option>
-      </select>
+      <div class="campo">
+        <input
+          type="text"
+          v-model="form.nome"
+          placeholder="Nome completo"
+        />
+      </div>
+      <div class="campo">
+        <input
+          type="email"
+          v-model="form.email"
+          placeholder="E-mail"
+        />
+      </div>
+       <div class="campo">
+        <select
+          v-model.number="form.cursoId"
+          :disabled="carregandoCursos"
+        >
+          <option :value="null" disabled>
+            Selecione o curso
+          </option>
+
+          <option
+            v-for="curso in cursos"
+            :key="curso.id"
+            :value="curso.id"
+          >
+            {{ curso.nome }}
+          </option>
+        </select>
+      </div>
       <button type="submit">Enviar</button>
     </form>
   </div>
 </template>
+
+<style scoped>
+
+input,
+select,
+button {
+  box-sizing: border-box;
+}
+
+.container {
+  width: 100%;
+  max-width: 380px;
+  padding: 30px;
+  border-radius: 14px;
+  background: #ffffff;
+  box-shadow: 0 12px 30px rgba(0, 0, 0, 0.08);
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 25px;
+  color: #2c3e50;
+}
+
+.campo {
+  margin-bottom: 18px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  font-size: 0.95rem;
+  transition: 0.2s ease;
+}
+
+input:focus,
+select:focus {
+  border-color: #42b883;
+  outline: none;
+  box-shadow: 0 0 0 3px rgba(66, 184, 131, 0.2);
+}
+
+button {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid transparent; /* 👈 adiciona isso */
+  border-radius: 8px;
+  background: #42b883;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.2s ease;
+}
+
+button:hover {
+  background: #369f6b;
+}
+
+button:disabled {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+</style>
